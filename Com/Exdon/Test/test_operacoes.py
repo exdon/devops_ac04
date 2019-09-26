910a249eb11439a43332871b0667a0330aef8265
Erik Xavier do Nascimento <53881701+exdon@users.noreply.github.com>
2019-09-26 18:55:24 -0300

    Update test_operacoes.py

--- Com/Exdon/Test/test_operacoes.py
+++ Com/Exdon/Test/test_operacoes.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
-from Com.Exdon.operacoes import Operacoes
+from Com.Exdon.operacoes import operacoes
 
 class TestOperacaoes(TestCase):
     def setUp(self):
-        self.operacoes = Operacoes()
+        self.operacoes = operacoes()
     
     def test_multiplicacao(self):
         self.assertEqual(self.operacoes.multiplicacao([5,5]), 25, "Deveria ser 25")
